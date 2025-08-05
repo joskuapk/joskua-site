c8f63d9b0291370b7057e8295203f36500aa4e49
joskua <joskua@billboardplanet.com>
2025-08-05 10:22:26 -0600

    feat: language switch

--- src/components/sections/Header.tsx
+++ src/components/sections/Header.tsx
@@ -1,7 +1,22 @@
+import Link from "next/link";
+
 export function Header() {
   return (
-    <header className="w-full px-6 py-4 flex items-center border-b border-light-blue bg-deep-blue">
+    <header className="w-full px-6 py-4 flex items-center justify-between border-b border-light-blue bg-deep-blue">
       <img src="/joskuaBanner.svg" alt="Joskua logo" className="h-10 w-auto" />
+      <div className="flex gap-3">
+        <Link href="/en">
+          <span className="text-xl" role="img" aria-label="English">
+            EN 🇺🇸
+          </span>
+        </Link>
+        <span>|</span>
+        <Link href="/es">
+          <span className="text-xl" role="img" aria-label="Español">
+            ES 🇲🇽
+          </span>
+        </Link>
+      </div>
     </header>
   );
 }
